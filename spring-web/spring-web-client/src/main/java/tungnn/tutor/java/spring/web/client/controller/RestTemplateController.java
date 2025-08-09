fc7417bbc9a45d0be1aebbae83ddae00d6cc92ea
tungnn <tungnn.hn@gmail.com>
2025-08-09 19:16:55 +0700

    spring-web: refactor

--- spring-web/spring-web-client/src/main/java/tungnn/tutor/java/spring/web/client/controller/RestTemplateController.java
+++ spring-web/spring-web-client/src/main/java/tungnn/tutor/java/spring/web/client/controller/RestTemplateController.java
@@ -13,37 +13,31 @@ public class RestTemplateController {
 
   @GetMapping("/get")
   public ResponseEntity<String> getExample() {
-      return restTemplate.getForEntity(API_URL, String.class);
+    return restTemplate.getForEntity(API_URL, String.class);
   }
 
   @PostMapping("/post")
   public ResponseEntity<String> postExample(@RequestBody String requestData) {
-      return restTemplate.postForEntity(API_URL, requestData, String.class);
+    return restTemplate.postForEntity(API_URL, requestData, String.class);
   }
 
   @PutMapping("/put/{id}")
   public void putExample(@PathVariable String id, @RequestBody String requestData) {
-      restTemplate.put(API_URL + "/{id}", requestData, id);
+    restTemplate.put(API_URL + "/{id}", requestData, id);
   }
 
   @DeleteMapping("/delete/{id}")
   public void deleteExample(@PathVariable String id) {
-      restTemplate.delete(API_URL + "/{id}", id);
+    restTemplate.delete(API_URL + "/{id}", id);
   }
 
   @GetMapping("/exchange")
   public ResponseEntity<String> exchangeExample() {
-      HttpHeaders headers = new HttpHeaders();
-      headers.setContentType(MediaType.APPLICATION_JSON);
+    HttpHeaders headers = new HttpHeaders();
+    headers.setContentType(MediaType.APPLICATION_JSON);
 
-      HttpEntity<String> entity = new HttpEntity<>(headers);
+    HttpEntity<String> entity = new HttpEntity<>(headers);
 
-      return restTemplate.exchange(
-          API_URL,
-          HttpMethod.GET,
-          entity,
-          String.class
-      );
+    return restTemplate.exchange(API_URL, HttpMethod.GET, entity, String.class);
   }
-  
 }
