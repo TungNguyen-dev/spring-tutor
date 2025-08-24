8efe842dc0b4d62406b2f0459adfd4803bafcc0b
tungnn <tungnn.hn@gmail.com>
2025-08-24 17:17:02 +0700

    hr-web-service: init project

---
+++ spring-capstone/hr-web-service/src/main/java/tungnn/tutor/java/spring/capstone/hr_web_service/HrWebServiceApplication.java
@@ -0,0 +1,13 @@
+package tungnn.tutor.java.spring.capstone.hr_web_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrWebServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrWebServiceApplication.class, args);
+	}
+
+}
---
+++ spring-capstone/hr-web-service/src/test/java/tungnn/tutor/java/spring/capstone/hr_web_service/HrWebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package tungnn.tutor.java.spring.capstone.hr_web_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrWebServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
