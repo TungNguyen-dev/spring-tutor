ab76494ecd88bfa75b32110255de7f64d4ef348f
tungnn <tungnn.hn@gmail.com>
2026-08-25 21:35:52 +0700

    Init project

---
+++ src/test/java/tungnn/tutor/java/spring/SpringTutorApplicationTests.java
@@ -0,0 +1,11 @@
+package tungnn.tutor.java.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTutorApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
