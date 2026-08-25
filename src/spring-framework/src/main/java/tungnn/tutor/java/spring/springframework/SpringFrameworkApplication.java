6ee8547dad6b129ec78ce9a193cd2c40744ab5c4
tungnn <tungnn.hn@gmail.com>
2026-08-25 21:12:47 +0700

    old

---
+++ src/spring-framework/src/main/java/tungnn/tutor/java/spring/springframework/SpringFrameworkApplication.java
@@ -0,0 +1,12 @@
+package tungnn.tutor.java.spring.springframework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFrameworkApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringFrameworkApplication.class, args);
+  }
+}
---
+++ src/spring-framework/src/test/java/tungnn/tutor/java/spring/springframework/SpringFrameworkApplicationTests.java
@@ -0,0 +1,11 @@
+package tungnn.tutor.java.spring.springframework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringFrameworkApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
